57326c7bc3786d3b2628871e0dd7ed74f2dcb446
Jatin Kaushal <92943010+jkaus324@users.noreply.github.com>
2024-03-25 20:31:38 +0530

    Different user type form added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,91 +1,42 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import { Footer, Navbar } from "../components";
 import { server } from "../index.js";
+import Student from "../components/Registration/Student.jsx";
+import Teacher from "../components/Registration/Teacher.jsx";
 
 const Register = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
+  const [activeForm, setActiveForm] = useState("student");
+  const [error, setError] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const response = await axios.post(`${server}/auth/register`, { name, email, password });
-            localStorage.setItem("email", email);
-            // Handle successful registration response
-            console.log(response.data);
-        } catch (error) {
-            // Handle error
-            setError("Failed to register. Please try again.");
-            console.error("Registration error:", error);
-        }
-    };
+    
+  };
 
-    return (
-        <>
-            <Navbar />
-            <div className="container my-3 py-3">
-                <h1 className="text-center">Register</h1>
-                <hr />
-                <div class="row my-4 h-100">
-                    <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
-                        <form onSubmit={handleSubmit}>
-                            <div class="form my-3">
-                                <label for="Name">User Name</label>
-                                <input
-                                    type="text"
-                                    class="form-control"
-                                    id="Name"
-                                    placeholder="Enter Your Name"
-                                    value={name}
-                                    onChange={(e) => setName(e.target.value)}
-                                    required
-                                />
-                            </div>
-                            <div class="form my-3">
-                                <label for="Email">Email address</label>
-                                <input
-                                    type="email"
-                                    class="form-control"
-                                    id="Email"
-                                    placeholder="name@example.com"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    required
-                                />
-                            </div>
-                            <div class="form  my-3">
-                                <label for="Password">Password</label>
-                                <input
-                                    type="password"
-                                    class="form-control"
-                                    id="Password"
-                                    placeholder="Password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    required
-                                />
-                            </div>
-                            {error && <p className="text-danger">{error}</p>}
-                            <div className="my-3">
-                                <p>Already have an account? <Link to="/login" className="text-decoration-underline text-info">Login</Link> </p>
-                            </div>
-                            <div className="text-center">
-                                <button class="my-2 mx-auto btn btn-dark" type="submit">
-                                    Register
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">Register</h1>
+        <hr />
+        <div class="row my-4 h-100">
+          <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">Select user type</div>
+          <select value={activeForm} className="col-md-4 col-lg-4 col-sm-8 mx-auto" onChange={(e) => setActiveForm(e.target.value)}>
+            <option value="student">Student</option>
+            <option value="teacher">Teacher</option>
+          </select>
+          <div>
+            {activeForm === "student" && <Student />}
+            {activeForm === "teacher" && <Teacher />}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Register;
